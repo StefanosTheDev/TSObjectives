c0905a8a8c47b981e85e34a408db954fc8d3e8cd
StefanosTheDev <stefanosdev26@gmail.com>
2025-01-15 21:22:57 -0800

    push

---
+++ TS/TS-1/basics.ts
@@ -0,0 +1,27 @@
+// All Types // Why is it const declroations may be initialized ?
+let _name: string;
+let _num: number;
+
+// Typescript Arrays
+let str_array: string[] = ['Apple', 'Banana'];
+// You can also do this with generic Array type
+let fruits: Array<string> = ['Apple', 'Orange'];
+// Array containing element of different data types using a generic array type syyntax
+let values: (string | number)[] = ['Apple', 3, 'Orange', 5];
+// Generic Approach Element
+let _values2: Array<string | number> = ['Apple', 9, 'Banana', 0];
+
+// We use Interfaces to add a layer of type safety by specifying what properties and types an object must have.
+// Do All objects i develop should have the interface type
+
+// ____________ Object Types _________________ //
+/*
+You can define an object with an interface right
+*/
+interface Person {
+  name: string;
+  age: number;
+}
+function greet(person: Person) {
+  return 'Hello' + person.name;
+}
